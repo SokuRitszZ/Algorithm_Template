7e7a04b1ad0505b4887530657769e0fcaf81b82d
SokuRitszZ <1062708758@qq.com>
2021-10-06 07:41:55 -0700

    add Template.cpp

---
+++ Code/Template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline int read(){
+  int x, flg = 1; char ch = getchar();
+  if (ch == '-') flg = -1;
+  while (ch < '0' or ch > '9') ch = getchar();
+  while ('0' <= ch and ch <= '9') x = (x << 3) + (x << 1) + (ch ^ 48), ch = getchar();
+  return x * flg;
+}
+
+signed main(){
+#ifdef LOCAL
+	freopen("in.in", "r", stdin);
+	freopen("out.out", "w", stdout);
+#endif 
+	// ---
+	
+	// --- 
+	
+	return 0;
+}
+
