f041d6c74b762ef9c680dbbc3d15433978915c04
SokuRitszZ <1062708758@qq.com>
2021-10-06 07:30:19 -0700

    add fastIO.cpp

---
+++ Code/Trick/fastIO.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+inline int read(){ 
+	int x, flg = 1; char ch = getchar();
+	if (ch == '-') flg = -1;
+	while (ch < '0' or ch > ''9') ch = getchar();
+	while ('0' <= ch and ch <= '9') x = (x << 3) + (x << 1) + (ch ^ 48), ch = getchar();
+	return x * flg;
+}
+	
