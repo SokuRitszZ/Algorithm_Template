59e6334ee268b78777028362248ed07edebf1461
SokuRitszZ <1062708758@qq.com>
2021-10-06 07:50:24 -0700

    modify fastIO

--- Code/Template.cpp
+++ Code/Template.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 inline int read(){
-  int x, flg = 1; char ch = getchar();
-  if (ch == '-') flg = -1;
-  while (ch < '0' or ch > '9') ch = getchar();
-  while ('0' <= ch and ch <= '9') x = (x << 3) + (x << 1) + (ch ^ 48), ch = getchar();
-  return x * flg;
+	int x = 0, flg = 1; char ch = getchar();
+	if (ch == '-') flg = -1;
+	while (ch < '0' or ch > ' 9') ch = getchar();
+	while ('0' <= ch and ch <= '9') x = (x << 3) + (x << 1) + (ch ^ 48), ch = getchar();
+	return x * flg;
 }
 
 signed main(){
--- Code/Trick/fastIO.cpp
+++ Code/Trick/fastIO.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 inline int read(){ 
-	int x, flg = 1; char ch = getchar();
+	int x = 0, flg = 1; char ch = getchar();
 	if (ch == '-') flg = -1;
 	while (ch < '0' or ch > '9') ch = getchar();
 	while ('0' <= ch and ch <= '9') x = (x << 3) + (x << 1) + (ch ^ 48), ch = getchar();
